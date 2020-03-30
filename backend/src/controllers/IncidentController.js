faea445548415770c34ce2424d89ad29a324f065
Luiz Felipe <raziel_libertino@hotmail.com>
2020-03-29 20:43:35 -0400

    Funcionalidades avançadas e tests

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -42,11 +42,14 @@ module.exports = {
         const { id } = req.params
         const ong_id = req.headers.authorization;
 
+
+
         const incident = await connection('incidents')
             .where('id', id)
             .select('ong_id')
             .first();
-
+        
+        if(!incident) return res.status(404).json({error: 'Not Found'})
         if(incident.ong_id !== ong_id) {
             return res.status(401).json({error: 'Operation not permitted'});
         }
